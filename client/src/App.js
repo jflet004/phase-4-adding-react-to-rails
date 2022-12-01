66adc579aa7675fc986a59bf5c5aced466ff056b
Jose Fletes <joseangelfletes@gmail.com>
2022-12-01 06:38:20 -0800

    commit tree 82e7cddb9b13f1401553cf632c579b75e003d035

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { useEffect } from "react"
+
+function App() {
+
+  useEffect(()=> {
+    fetch("/movies")
+    .then(res => res.json())
+    .then(movies => console.log(movies))
+  }, [])
+  return (
+    <div className="App">
+      <h1>Hello from React!</h1>
+    </div>
+  );
+}
+
+export default App;
